ad3f416e0b2ae102782b200dfcc53528af210dea
twodzinski@op.pl <twodzinski@op.pl>
2023-04-23 09:53:31 +0200

    App

--- shop/src/pl/comarch/it/camp/tomwodz/shop/App.java
+++ shop/src/pl/comarch/it/camp/tomwodz/shop/App.java
@@ -5,11 +5,6 @@ import pl.comarch.it.camp.tomwodz.shop.core.Core;
 
 public class App {
     public static void main(String[] args) {
-
-    /*    String password = "TomekuxU9xUQWQMbZQm6rmtHd";
-        String hash = DigestUtils.md5Hex(password);
-        System.out.println(hash);*/
-
         System.out.println("Welcome in our GardenShop!");
         Core.start();
     }
